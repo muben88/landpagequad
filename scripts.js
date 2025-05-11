c1bad2d717b6d30686dae1f20e1b19d784b9187a
Mustapha <ga4tech@gmail.com>
2025-05-11 22:12:40 +0100

    edited the menu

--- scripts.js
+++ scripts.js
@@ -65,3 +65,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
       });
     });
+
+ document.addEventListener("DOMContentLoaded", function() {
+    const toggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("show");
+    });
+  });
